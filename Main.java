4be05e9f0259bb60819eafef74abab27c498c6e8
Jitesh Ramesh Sanap <jiteshrsanap123@gmail.com>
2025-04-12 10:45:54 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main{
         System.out.println("this is java program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("hello jenkins");
     }
 }
